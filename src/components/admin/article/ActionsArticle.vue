923896c193bf0053206ae9b1f18f84c2244b8918
Ezra Yoanda Nicolas <ezrayoandanicolas@gmail.com>
2023-08-20 16:40:42 +0700

    fix git repository

--- src/components/admin/article/ActionsArticle.vue
+++ src/components/admin/article/ActionsArticle.vue
@@ -78,7 +78,7 @@ export default {
                     </div>
                     <div class="w-full md:w-1/2 px-3">
                         <label class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" for="grid-slug">
-                            Slug
+                            Slug Article
                         </label>
                         <input class="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500" id="grid-slug" type="text" placeholder="Slug">
                     </div>
